8ad2aac8405db99c80453f54a8c311f97e54bb77
w-v <wtv@protonmail.ch>
2018-09-21 17:38:02 +0200

    comments

--- src/deepjsfun.js
+++ src/deepjsfun.js
@@ -1,44 +1,55 @@
+
+
 /* TODO:
  * - make it faster
  * - more accurate 
  * - consistent start (avoid starting at loss = 0.8 and a color missing etc.)
- * - clean the code
- * - asynchronous updates (if even possible)
- * - display loss chart, value
- * - interactivity
+ * - clean the code : split it in functions, put in good variable names
+ * - asynchronous image updates (if even possible) : update the image without stopping the learning process
+ * - display loss chart, make a nice tab for loss values etc
+ * - interactivity : model layout inputed in a form, some sliders/forms for batch size etc, training has to be restarted when those settings are tweaked. A learning rate slider, possibly without reloading the model
  */
-const updating = '<strong id="updating">updating</strong>';
-const canvas = document.getElementById("orig");
-const ctx = canvas.getContext("2d");
+
+// original image
+img = new Image;
+img.src = "img/lena.jpg";
+
 const w= 200;
 const h= 200;
 
-img = new Image;
-img.src = "img/lena.jpg";
-//img.src = "img/kitty.jpg";
-//var ys = [];
+// to get raw image data
+const canvas = document.getElementById("orig");
+const ctx = canvas.getContext("2d");
+
 img.onload = () => {
+  // image is loaded
+
   ctx.drawImage(img, 0, 0);
   imageData = ctx.getImageData(0, 0, w,h);
-  console.log(imageData);
   train(imageData);
-  //while(imageData.data.length) ys.push(imageData.data.prototype.splice(0,3)); 
 }
-//tys;
+
+
 function train(imageData) {
-  console.log(w);
+
+  /* generate pixel coords
+   * makes a 2d array of wize h,w containing every pixel coordinates
+   * [[0,0],[0,1]...[0,h],
+   *  [1,0],  ...  ,[1,h],
+   *  [w,0],  ...  ,[w,h]]
+   * is used as input to the model
+   */
   var xs = [];
   for(i=0;i<w;i++){
     for(j=0;j<h;j++){
       xs.push([i,j]);
     }
   }
-  //  shuffle(xs);
 
-  console.log(img);
   // Define a model for linear regression.
   const model = tf.sequential();
-  //model.add(tf.input({shape: [2]}));
+  // model layout is similar ConvNetJs' model
+  // 2 inputs : x,y
   model.add(tf.layers.dense({units: 20, inputShape: [2], activation: 'relu', kernelInitializer: 'varianceScaling'}));
   model.add(tf.layers.dense({units: 20, activation: 'relu', kernelInitializer: 'varianceScaling'}));
   model.add(tf.layers.dense({units: 20, activation: 'relu', kernelInitializer: 'varianceScaling'}));
@@ -46,28 +57,52 @@ function train(imageData) {
   model.add(tf.layers.dense({units: 20, activation: 'relu', kernelInitializer: 'varianceScaling'}));
   model.add(tf.layers.dense({units: 20, activation: 'relu', kernelInitializer: 'varianceScaling'}));
   model.add(tf.layers.dense({units: 3, activation: 'relu', kernelInitializer: 'varianceScaling'}));
+  // 3 outputs : rgb
 
+  // did not tinker much with that
   const LEARNING_RATE = 0.1;
+
+  // ConvNetJS has a momentum variable, so the optimizer was chosen accordingly
   const MOMENTUM = 0.9;
-  //const optimizer = tf.train.sgd(LEARNING_RATE);
   const optimizer = tf.train.momentum(LEARNING_RATE,MOMENTUM);
 
   // Prepare the model for training: Specify the loss and the optimizer.
   model.compile({loss: 'meanSquaredError', optimizer: optimizer});
 
+  // make a tensor from the pixel coords array, shape is inferred from the original array : [w*h,2]
   var txs = tf.tensor(xs);
+
+  /* It apparently is a good idea to normalize the data, so coordinates are mapped from
+   * 0-200 to -0.5,0.5
+   */
   txs = txs.sub(tf.scalar(w/2));
   txs = tf.cast(txs,'float32');
   txs = txs.div(tf.scalar(w));
+
+  /* put raw image data (array of bytes) into a tensor of shape [h,w,3]
+   * reshape it to [h*w,3] so that each coordinates in txs corresponds
+   * to a pixel
+   */
   var tys = tf.fromPixels(imageData, 3).reshape([w*h,3]);
+
+  // map 0-255 int to 0-1 float
   tys = tf.cast(tys,'float32');
   tys = tys.div(tf.scalar(255));
-  // const tys = tf.tensor(ys);
-  console.log("lalal");
-  tys.print();
-  // Train the model using the data.
-  //txs = tf.tensor2d([0,0,0,1,1,0,1,1],[4,2]);
-  //tys = tf.tensor2d([0,0,0,1,1,1,2,2,2,3,3,3],[4,3]);
+
+  /* Train the model using the data.
+   * 
+   * batchSize is the number of outputs to be computed before weights are updated
+   * smaller batch size is slower, although the model will learn more in an epoch
+   *
+   * validationSplit is the ratio of data reserved for validation testing, the model 
+   * will not be trained on it, we don't want any
+   *
+   * shuffle allows for batches inputs to be picked randomly, making it learn adjacent pixels
+   * in a batch is somewhat biased
+   *
+   * callbacks are a list of function to be called at the end/beginning of each epoch/batch
+   * it is used to update the image during the training
+   */
   model.fit(txs, tys, { batchSize: 250, epochs: 4000, validationSplit: 0, shuffle: true,callbacks: {
     onTrainBegin: async () => {
       console.log("onTrainBegin")
@@ -80,35 +115,44 @@ function train(imageData) {
     },
     onEpochEnd: (epoch, logs) => {
       console.log("onEpochEnd" + epoch + JSON.stringify(logs))
+      
+      // print epoch number inside tag having id #epoch
       $('#epoch')[0].innerHTML=epoch;
-      //$('head')[0].after(updating);
-      console.log(txs);
+
+      // every 3 epoch, update the result image
       if(epoch%3 != 0 && epoch > 0)
         return;
-      txs.print();
+
+      // get the model outputs for all pixel coordinates
       img_out = model.predict(txs, {batchSize: 1000, verbose: true});//);
-      img_out.print();
+
+      // reformat it to bytes
       img_out = img_out.mul(tf.scalar(255));
-      img_out.print();
       img_out = tf.cast(img_out,'int32');
-      img_out.print();
+
+      // actually request the data (model.predict gives a promise)
       img_out_data = Array.from(img_out.dataSync());
 
-      console.log(img_out_data);
+      // for some reason a canvas only takes 4 channel images
+      // so the Alpha channel has to be inserted
+      // this is hugely inefficient
+      // Chrome takes ~0.5s 
+      // Firefox takes ~2s and complains about the script being frozen 
       for(i=3;i<img_out_data.length+1;i+=4){
         img_out_data.splice(i,0,255);
       }
-      console.log(img_out_data);
 
+      // ImageData must be constructed using this typed array
       img_out_data = Uint8ClampedArray.from(img_out_data);
-
-      console.log(img_out_data);
-
       imagedata = new ImageData(img_out_data,w,h);
 
+
+      // get result canvas
       const canvas = document.getElementById("result");
       const ctx = canvas.getContext("2d");
 
+      // trickery to draw the image on the canvas
+      // ref : https://stackoverflow.com/questions/24236470/html5-can-i-create-an-image-object-from-an-imagedata-object
       var tmpcanvas = document.createElement('canvas');
       var tmpctx = tmpcanvas.getContext('2d');
       tmpcanvas.width = canvas.width;
@@ -126,28 +170,11 @@ function train(imageData) {
       //    console.log("onBatchBegin" + epoch + JSON.stringify(logs))
     },
     onBatchEnd: async (epoch, logs) => {
-      //   console.log("onBatchEnd" + epoch + JSON.stringify(logs))
+      // print log infos (loss etc) in the tag having id #epoch
       $('#loss')[0].innerHTML=JSON.stringify(logs);
     }
   }
   }).then(() => {
-    // Use the model to do inference on a data point the model hasn't seen before:
-    // Open the browser devtools to see the output
     console.log("done")
   });
-}
-
-/**
- * Shuffles array in place.
- * @param {Array} a items An array containing the items.
- */
-function shuffle(a) {
-  var j, x, i;
-  for (i = a.length - 1; i > 0; i--) {
-    j = Math.floor(Math.random() * (i + 1));
-    x = a[i];
-    a[i] = a[j];
-    a[j] = x;
-  }
-  return a;
 }
