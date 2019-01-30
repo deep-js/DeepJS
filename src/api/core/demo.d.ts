4041a95bed9048b11502e195757ad4bf8bede11b
18002769 <Marseillaise29>
2019-01-30 17:39:00 +0100

    supression Majuscule sur le fichier json

--- src/api/core/demo.d.ts
+++ src/api/core/demo.d.ts
@@ -1,2 +1,3 @@
 export * from './demo/demo.presenter'
 export * from './demo/demo.component'
+export * from './demo/input'
--- src/api/core/demo/input.d.ts
+++ src/api/core/demo/input.d.ts
@@ -1,2 +1,4 @@
 export * from './input/input.presenter'
 export * from './input/input.component'
+export * from  './input/jsonInput'
+export * from  './input/imageInput'
