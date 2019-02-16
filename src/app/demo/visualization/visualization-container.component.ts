c424bfc7e9d391f27fa809118d76441f0b596542
wtv <wtv@protonmail.ch>
2019-02-16 22:28:24 +0100

    visualization-container comments

--- src/app/demo/visualization/visualization-container.component.ts
+++ src/app/demo/visualization/visualization-container.component.ts
@@ -6,6 +6,11 @@ import { VisualizationContainerComponent } from '@api/core';
   templateUrl: './visualization-container.component.html',
   styleUrls: ['./visualization-container.component.css']
 })
+
+/**
+ * Implementation for VisualizationContainerComponent
+ *
+ */
 export class VisualizationContainerComponentImpl implements VisualizationContainerComponent, OnInit {
 
   constructor() { }
--- src/app/demo/visualization/visualization-item/visualization-item.presenter.ts
+++ src/app/demo/visualization/visualization-item/visualization-item.presenter.ts
@@ -1,5 +1,9 @@
 import {VisualizationItemPresenter} from '@api/core';
 
+/**
+ * Implementation for VisualizationItemPresenter
+ *
+ */
 export class VisualizationItemPresenterImpl implements VisualizationItemPresenter {
 
   constructor() { }
