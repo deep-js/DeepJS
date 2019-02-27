b9bd40c86fc30d89fd2ae449257413206071c678
wtv <wtv@protonmail.ch>
2019-02-27 16:08:21 +0100

    image input api comments

--- src/api/core/demo/input/image-input/image-input.component.ts
+++ src/api/core/demo/input/image-input/image-input.component.ts
@@ -2,6 +2,9 @@ import {InputComponent} from '../input.component'
 
 /**
  * Component for importing image datasets
+ * Includes a graphical interface allowing the user to give a list of files
+ * that will be used as input for the model
+ * Passes this list to its presenter who converts it to tensors
  */
 export interface ImageInputComponent extends InputComponent{
 
