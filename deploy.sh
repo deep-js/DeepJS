4698ef668b0d3a9f29bf8bf8447b83d88b836b63
w-v <wtv@protonmail.ch>
2019-05-04 18:19:46 +0200

    publish compodoc to doc repo

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
-cd documentation
-git add . && \
+git clone https://github.com/deep-js/doc.git
+mv documentation/* doc/
+cd doc
+git status
+git add -A && \
 git commit -m "Deploy to GitHub Pages" && \
 git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
