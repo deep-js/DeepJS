444554a65392d9922d6dbed20139e46508f2bb14
w-v <34216849+w-v@users.noreply.github.com>
2019-05-04 17:35:15 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 cd documentation
 git add . && \
 git commit -m "Deploy to GitHub Pages" && \
-git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
