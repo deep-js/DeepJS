e9abc9a757defeae6e9c51dd97fb488dfc5a9eca
wtv <wtv@protonmail.ch>
2019-05-06 23:12:40 +0200

    further testing on tensorspace integration

--- src/app/demo/model/ts-model/ts-model.presenter.ts
+++ src/app/demo/model/ts-model/ts-model.presenter.ts
@@ -56,6 +56,28 @@ export class TSModelPresenterImpl implements api.TSModelPresenter{
    * #uglyhack
    */
   private evaluate(s:string) {
+    const model = tf.sequential();
+    model.add(tf.layers.conv2d({
+        inputShape: [28, 28, 1],
+        kernelSize: 3,
+        filters: 16,
+        activation: 'relu'
+    }));
+    model.add(tf.layers.maxPooling2d({poolSize: 2, strides: 2}));
+    model.add(tf.layers.conv2d({kernelSize: 3, filters: 32, activation: 'relu'}));
+    model.add(tf.layers.maxPooling2d({poolSize: 2, strides: 2}));
+    model.add(tf.layers.conv2d({kernelSize: 3, filters: 32, activation: 'relu'}));
+    model.add(tf.layers.flatten({}));
+    model.add(tf.layers.dense({units: 64, activation: 'relu'}));
+    model.add(tf.layers.dense({units: 10, activation: 'softmax'}));
+    const optimizer = 'rmsprop';
+    model.compile({
+        optimizer,
+        loss: 'categoricalCrossentropy',
+        metrics: ['accuracy'],
+    });
+    return model;
+
     if (s == null)
       throw new ModelDefBoxEmptyError();
     
