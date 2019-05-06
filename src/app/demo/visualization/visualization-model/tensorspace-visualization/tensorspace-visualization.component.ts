19e6c9d17e2d964d26356f6631e35c9f591c2bc1
w-v <wtv@protonmail.ch>
2019-05-06 08:39:06 +0200

    toucassé

--- src/app/demo/visualization/visualization-model/tensorspace-visualization/tensorspace-visualization.component.ts
+++ src/app/demo/visualization/visualization-model/tensorspace-visualization/tensorspace-visualization.component.ts
@@ -4,6 +4,7 @@ import { TrainerServiceImpl, TrainerService } from '../../../../shared/services/
 import {VisualizationModelPresenter, VisualizationModelComponent} from '@api/core';
 import { TensorspaceVisualizationPresenter } from './tensorspace-visualization.presenter';
 import * as TSP from 'tensorspace';
+import * as tf from '@tensorflow/tfjs';
 
 
 /**
@@ -20,9 +21,11 @@ export class TensorspaceVisualizationComponent implements AfterViewInit, Visuali
 
   presenter: VisualizationModelPresenter;
   @ViewChild('tensorspace') tensorspaceContainer; 
+  modelTrainer: TrainerServiceImpl;
 
   constructor(modelTrainer: TrainerServiceImpl) {
     this.presenter = new TensorspaceVisualizationPresenter(modelTrainer);
+    this.modelTrainer = modelTrainer;
 
 
     /*
@@ -41,33 +44,85 @@ export class TensorspaceVisualizationComponent implements AfterViewInit, Visuali
     /*
     let container = document.getElementById( "tensorspaceContainer" );
     let model = new tsp.models.Sequential( container );
-    model.add( new tsp.layers.GreyscaleInput({ shape: [28, 28, 1] }) );
-    model.add( new tsp.layers.Padding2d({ padding: [2, 2] }) );
-    model.add( new tsp.layers.Conv2d({ kernelSize: 5, filters: 6, strides: 1 }) );
-    model.add( new tsp.layers.Pooling2d({ poolSize: [2, 2], strides: [2, 2] }) );
-    model.add( new tsp.layers.Conv2d({ kernelSize: 5, filters: 16, strides: 1 }) );
-    model.add( new tsp.layers.Pooling2d({ poolSize: [2, 2], strides: [2, 2] }) );
-    model.add( new tsp.layers.Dense({ units: 120 }) );
-    model.add( new tsp.layers.Dense({ units: 84 }) );
-    model.add( new tsp.layers.Output1d({
-      units: 10,
-      outputs: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    }) );
+    model.add( new tsp.layers.Conv2d() );
+    model.add( new tsp.layers.Pooling2d() );
+    model.add( new tsp.layers.Conv2d() );
+    model.add( new tsp.layers.Pooling2d() );
+    model.add( new tsp.layers.Conv2d() );
+    model.add( new tsp.layers.Flatten() );
+    model.add( new tsp.layers.Dense() );
+    model.add( new tsp.layers.Dense() );
 
     model.init();
      */
   }
 
-  ngAfterViewInit() {
+
+  convert(model:tf.Model):tf.Model{
+    var outputList = [];
+    for ( let i = 0; i < model.layers.length; i ++ ) {
+          outputList.push( model.layers[i].output );
+    }
+    const inputs = tf.input({shape: [28,28,1]}) ;
+    console.log(inputs);
+    return tf.model({inputs: model.inputs, outputs: outputList});
+  
+  }
+
+  render(omodel:tf.Model){
+    /*var mm = tf.sequential();
+    mm.add(tf.layers.conv2d({ inputShape: [28, 28, 1],kernelSize: 3, filters: 16,activation: 'relu'}));
+    mm.add(tf.layers.maxPooling2d({poolSize: 2, strides: 2}));
+    mm.add(tf.layers.conv2d({kernelSize: 3, filters: 32, activation: 'relu'}));
+    mm.add(tf.layers.maxPooling2d({poolSize: 2, strides: 2}));
+    mm.add(tf.layers.conv2d({kernelSize: 3, filters: 32, activation: 'relu'}));
+    mm.add(tf.layers.flatten({}));
+    mm.add(tf.layers.dense({units: 64, activation: 'relu'}));
+    mm.add(tf.layers.dense({units: 10, activation: 'softmax'}));
+    const optimizer = 'rmsprop';
+    mm.compile({ optimizer,loss: 'categoricalCrossentropy', metrics: ['accuracy'],});*/
     let modelContainer = document.getElementById("tensorspaceContainer");
     let model = new TSP.models.Sequential(modelContainer);
-
-    model.add(new TSP.layers.GreyscaleInput({ shape: [28, 28, 1] }));
-    model.add(new TSP.layers.Padding2d({ padding: [2, 2] }));
-    model.add(new TSP.layers.Conv2d({ kernelSize: 5, filters: 6, strides: 1 }));
-    model.add(new TSP.layers.Pooling2d({ poolSize: [2, 2], strides: [2, 2] }));
+    model.add( new TSP.layers.Conv2d({ inputShape: [28, 28, 1],kernelSize: 3, filters: 16,activation: 'relu'}) );
+    model.add( new TSP.layers.Pooling2d({poolSize: 2, strides: 2}) );
+    model.add( new TSP.layers.Conv2d({kernelSize: 3, filters: 32, activation: 'relu'}) );
+    model.add( new TSP.layers.Pooling2d({poolSize: 2, strides: 2}) );
+    model.add( new TSP.layers.Conv2d({kernelSize: 3, filters: 32, activation: 'relu'}) );
+    model.add( new TSP.layers.Flatten() );
+    /*var mm = tf.sequential();
+    mm.add(tf.layers.dense({inputShape: [64], units: 64, activation: 'relu'}));
+    mm.add(tf.layers.dense({units: 10, activation: 'softmax'}));*/
+    model.add( new TSP.layers.Dense({units: 64, activation: 'relu'}) );
+    model.add( new TSP.layers.Dense({units: 10, activation: 'softmax'}) );
+    //let outmodel = this.convert(mm);//new TSP.models.Sequential(modelContainer);
+    model.load({
+      type: 'live',
+      modelHandler: omodel
+    })
 
     model.init();
+  } 
+
+  ngAfterViewInit() {
+
+
+    /*
+    let modelContainer = document.getElementById("tensorspaceContainer");
+    let tmodel = TSP.models.Sequential( modelContainer );
+    tmodel.add(new TSP.layers.GreyscaleInput({ shape: [28, 28, 1] }));
+    tmodel.add(new TSP.layers.Padding2d({ padding: [2, 2] }));
+    tmodel.add(new TSP.layers.Conv2d({ kernelSize: 5, filters: 6, strides: 1 }));
+    tmodel.add(new TSP.layers.Pooling2d({ poolSize: [2, 2], strides: [2, 2] }));
+    //tspModelSeq.layers = model.layers;
+    tmodel.init();*/
+
+    
+    this.modelTrainer.getCurrentTrainings$().subscribe( (training) => this.render(training.getModel()) )
+
+
+
+
+
   }
 
 }
